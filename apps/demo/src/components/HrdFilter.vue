075ed1c0c8db8453f9741d6a88b27ba93f671f4e
Alexander Vu <alexander.vu.tuyet@gmail.com>
2022-07-17 13:04:37 +0200

    make properties dependence visible

--- apps/demo/src/components/HrdFilter.vue
+++ apps/demo/src/components/HrdFilter.vue
@@ -10,7 +10,7 @@
     <HrdSwitch id="switch-1" v-model="store.isHeaderVisible" title="Header visible">
       Show or hide the Header with all his functionality.
     </HrdSwitch>
-    <HrdSwitch id="switch-2" v-model="store.isHeaderStatic" title="Static header">
+    <HrdSwitch id="switch-2" v-if="store.isHeaderVisible" v-model="store.isHeaderStatic" title="Static header">
       Make the Header static to stick on the top when scrolling.
     </HrdSwitch>
     <HrdSwitch id="switch-3" v-model="store.isFirstColumnStatic" title="Static first column">
@@ -19,11 +19,11 @@
     <HrdSwitch id="switch-4" v-model="store.isLastColumnStatic" title="Static last column">
       Make the last column static to stick on the right when scrolling horizontally.
     </HrdSwitch>
-    <HrdSwitch id="switch-6" v-model="store.isSortable" title="Sortable">
+    <HrdSwitch id="switch-6" v-if="store.isHeaderVisible"  v-model="store.isSortable" title="Sortable">
       Show <strong>sort icon</strong> in every header column to enable sorting.
       The sort icon can be disabled for every column in field settings.
     </HrdSwitch>
-    <HrdSwitch id="switch-7" v-model="store.isSettingsEnabled" title="Has Settings">
+    <HrdSwitch id="switch-7" v-if="store.isHeaderVisible" v-model="store.isSettingsEnabled" title="Has Settings">
       Show the settings icon in header on the last column to show the settings view.
     </HrdSwitch>
     <HrdSwitch id="switch-5" v-model="store.isSelectable" title="Selectable"/>
