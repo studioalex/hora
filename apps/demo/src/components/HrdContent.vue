b040e0e9a258b3128ebe70a5e814450c4f372a5c
Alexander Vu <alexander.vu.tuyet@gmail.com>
2022-07-17 12:11:39 +0200

    add headline to demo content

--- apps/demo/src/components/HrdContent.vue
+++ apps/demo/src/components/HrdContent.vue
@@ -12,22 +12,23 @@
 </script>
 
 <template>
-  <div class="hrd-content__grid">
-    <hora-grid
-      :fields="data.columns"
-      :data="data.data"
-      :is-header-static="store.isHeaderStatic"
-      :is-first-field-static="store.isFirstColumnStatic"
-      :is-last-field-static="store.isLastColumnStatic"
-      :is-header-visible="store.isHeaderVisible"
-      :is-selectable="store.isSelectable"
-      :is-sortable="store.isSortable"
-      :is-settings-enabled="store.isSettingsEnabled"
-      :single-selection="testSingleSelection"
-      @onSelection="setSelected">
-      <template #cell-k1="{ record, field }">
-        {{ field }} -- {{ record }}
-      </template>
-    </hora-grid>
+  <div class="hrd-grid-content">
+    <h1>Grid Example</h1>
+      <hora-grid
+        :fields="data.columns"
+        :data="data.data"
+        :is-header-static="store.isHeaderStatic"
+        :is-first-field-static="store.isFirstColumnStatic"
+        :is-last-field-static="store.isLastColumnStatic"
+        :is-header-visible="store.isHeaderVisible"
+        :is-selectable="store.isSelectable"
+        :is-sortable="store.isSortable"
+        :is-settings-enabled="store.isSettingsEnabled"
+        :single-selection="testSingleSelection"
+        @onSelection="setSelected">
+        <template #cell-k1="{ record, field }">
+          {{ field }} -- {{ record }}
+        </template>
+      </hora-grid>
   </div>
 </template>
