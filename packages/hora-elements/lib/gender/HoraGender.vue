e5c6b8e03276192492f16e1319252f84a4a55036
Alexander Sedeke <alexander.sedeke@gmail.com>
2022-09-04 13:36:35 +0200

    fix component naming

---
+++ packages/hora-elements/lib/gender/HoraGender.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+  import { toRefs } from 'vue'
+  const props = defineProps({
+    gender: {
+      type: String,
+      default: 'none'
+    },
+    caption: {
+      type: String,
+      default: undefined
+    }
+  })
+
+  const { gender } = toRefs(props)
+</script>
+
+<template>
+  <div>
+    {{ gender }}
+  </div>
+</template>
